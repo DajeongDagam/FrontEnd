d24a46e14cbc28dfa2b86a50aacb59c5ad4340c5
yunhee1 <alkrg007@naver.com>
2023-08-14 01:41:04 +0900

    Feat: 로그인 화면 배치

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LandingPage from "./pages/LandingPage";
 import DictionaryPage from "./pages/DictionaryPage";
 import MyPage from "./pages/MyPage";
+import LoginPage from "./pages/LoginPage";
 import { Route, Routes } from "react-router-dom";
 
 import React from "react";
@@ -8,8 +9,9 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/" element={<DictionaryPage />} />
-      <Route path="/" element={<MyPage />} />
+      {/* <Route path="/" element={<DictionaryPage />} />
+      <Route path="/" element={<MyPage />} /> */}
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
