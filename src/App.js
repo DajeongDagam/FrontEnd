4e8c33b824c55b370f4f43cd82faaafe731b731e
ChaeYubin <cyoobin@naver.com>
2023-08-14 00:36:27 +0900

    Feat: react-router-dom 라이브러리 추가 및 라우터 처리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import LandingPage from "./pages/LandingPage";
+import DictionaryPage from "./pages/DictionaryPage";
+import MyPage from "./pages/MyPage";
+import { Route, Routes } from "react-router-dom";
 
 import React from "react";
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<DictionaryPage />} />
+      <Route path="/" element={<MyPage />} />
+    </Routes>
   );
 }
 
