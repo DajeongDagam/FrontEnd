3428b1c056e366f9b357a40156531549cbb8ddce
yunhee1 <alkrg007@naver.com>
2023-08-13 16:24:13 +0900

    Feat: 상단 바 타이틀, drawer 버튼 배치

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../css/container.scss";
+import drawbar_btn from "../images/draw-btn.png";
 
 const LandingPage = () => {
   return (
     <div className="background">
-      <div className="container">랜딩페이지</div>
+      <div className="container">
+        <div className="top-bar-container">
+          <div className="title-container">다가감</div>
+          <div className="draw-btn-container">
+            <img src={drawbar_btn} alt="햄버거버튼" className="drawbar-btn" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
