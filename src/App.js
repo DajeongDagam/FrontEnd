1a903cd4569611014d765a1e7d4adf23974526e0
yunhee1 <alkrg007@naver.com>
2023-08-16 22:57:07 +0900

    Fix: conflit 해결

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dictionary/edit" element={<DictionaryPage />} />
       <Route path="/dictionary/search" element={<DictionaryPage />} />
-      {/* <Route path="/drawer" element={<Drawer />} /> */}
     </Routes>
   );
 }
